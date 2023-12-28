d8d05c21d779a464cc8b74e6e3e25bf3c6ebc566
ukarpenkov <truloveu@gmail.com>
2023-12-29 00:15:28 +0300

    try to progress info

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,22 @@
 import { WordCard } from "./components/WordCard/WordCard";
-import { Test } from "./components/WordCard/test";
-import { AppRoot, Panel, View } from '@vkontakte/vkui';
+
+import { AppRoot, Counter, Panel, View } from '@vkontakte/vkui';
 
 function App() {
   return (
+
     <AppRoot>
 
-      <View id="main" activePanel="general">
-        <Panel id="general">
-          <Test />
+      <View id="main" activePanel="round">
+        <Panel id="round">
+          <WordCard />
         </Panel>
 
       </View>
     </AppRoot>
+
+
+
   );
 }
 
--- src/components/WordCard/WordCard.jsx
+++ src/components/WordCard/WordCard.jsx
@@ -1,28 +1,36 @@
 import {
-  View,
-  Panel,
-  PanelHeader,
-  Header,
-  Group,
-  CardGrid,
-  Card,
+  AdaptivityProvider, Button, ButtonGroup, Card, CardGrid, Counter, Image, ModalCardBase, ViewWidth,
+
 } from '@vkontakte/vkui';
+import { Icon20CancelCircleFillRed, Icon20CheckCircleOn, Icon24Cancel, Icon24LogoVk } from '@vkontakte/icons';
 
 export const WordCard = () => {
   return (
-    <View activePanel="wordCard">
-      <Panel id="wordCard">
-        <PanelHeader>Card</PanelHeader>
-        <Group>
-          <Group mode="plain" header={<Header mode="secondary">Дефолтный стиль</Header>}>
-            <CardGrid size="l">
-              <Card>
-                <div style={{ height: 96 }} />
-              </Card>
-            </CardGrid>
-          </Group>
-        </Group>
-      </Panel>
-    </View>
+    <>
+      <ModalCardBase
+        dismissButtonMode='undefined'
+        style={{ width: 320 }}
+        header="Заноза"
+        actions={
+          <ButtonGroup mode="horizontal" gap="s" stretched>
+            <Button size="l" mode="primary" stretched>
+              <Icon20CancelCircleFillRed width={20} height={20} />
+
+            </Button>
+            <Button size="l" mode="primary" stretched>
+              <Icon20CheckCircleOn width={20} height={20} color='blue' />
+            </Button>
+          </ButtonGroup>
+        }
+      />
+      <div className='current-result'>
+        <Button size="m" after={<Counter color='white' mode='primary'>8</Counter>} style={{ backgroundColor: 'red' }}>
+          Пропущено
+        </Button>
+        <Button size="m" after={<Counter>20</Counter>}>
+          Отгадано
+        </Button>
+      </div >
+    </>
   )
 }

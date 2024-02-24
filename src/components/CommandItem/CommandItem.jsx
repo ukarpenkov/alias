a2823da83adb346ca390b988dd2c55c80f242690
ukarpenkov <truloveu@gmail.com>
2024-02-24 12:26:45 +0300

    score styles

--- src/components/CommandItem/CommandItem.jsx
+++ src/components/CommandItem/CommandItem.jsx
@@ -4,7 +4,7 @@ import {
 } from '@vkontakte/vkui';
 import { useDispatch, useSelector } from 'react-redux'
 import { Icon16Cancel, } from '@vkontakte/icons';
-
+import './CommandItem.css'
 
 
 export const CommandItem = ({ name, id, score, removeCommand, editCommand, result, commandsCount }) => {
@@ -29,14 +29,14 @@ export const CommandItem = ({ name, id, score, removeCommand, editCommand, resul
       {commandsCount <= 2 ?
         <Button size="l"
           disabled={(result === 'false') ? false : true}
-          appearance="accent" before={(result === 'false') ? <Icon16Cancel /> : score} />
+          appearance="accent" className='score-info' before={(result === 'false') ? <Icon16Cancel /> : `${score}`} />
         :
-        <Button size="l"
+        <Button size="l" className='score-info'
           disabled={(result === 'false') ? false : true}
           onClick={() => {
             removeCommand(id)
           }}
-          appearance="accent" before={(result === 'false') ? <Icon16Cancel /> : score} />
+          appearance="accent" before={(result === 'false') ? <Icon16Cancel /> : `${score}`} />
 
       }
 

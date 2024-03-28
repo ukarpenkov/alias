1b0822cca949a449ec6750d1215f1053f2c2a3b2
ukarpenkov <truloveu@gmail.com>
2024-03-28 20:31:26 +0300

    del imports

--- src/components/Timer/Timer.jsx
+++ src/components/Timer/Timer.jsx
@@ -1,19 +1,9 @@
-import React from 'react';
-import { useSelector } from 'react-redux'
-import {
-  Button,
-} from '@vkontakte/vkui';
-import { Icon20Clock } from '@vkontakte/icons';
-
 export const Timer = ({ s }) => {
-
-  console.log(s.toString().padStart(2, '0'))
   return (
     <div>
       <p>
         {s.toString().padStart(2, '0')}
       </p>
     </div>
-
   )
 }

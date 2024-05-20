aed9a34af83dd2a73e507576c6d3a9e599bac63f
ukarpenkov <truloveu@gmail.com>
2024-05-20 21:39:14 +0300

    card to command name

--- src/components/CommandItem/CommandItem.jsx
+++ src/components/CommandItem/CommandItem.jsx
@@ -1,41 +1,61 @@
-import {
-  Button, Input,
-} from '@vkontakte/vkui';
+import { Button, Card, Input } from "@vkontakte/vkui";
 
-import { Icon16Cancel, } from '@vkontakte/icons';
-import './CommandItem.css'
-import { useEffect } from 'react';
-import { useDispatch, } from 'react-redux'
+import { Icon16Cancel } from "@vkontakte/icons";
+import "./CommandItem.css";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
 
-export const CommandItem = ({ name, id, score, removeCommand, editCommand, result, commandsCount, currentScore }) => {
-
+export const CommandItem = ({
+  name,
+  id,
+  score,
+  removeCommand,
+  editCommand,
+  result,
+  commandsCount,
+  currentScore,
+}) => {
   return (
-    <div className='command-card'>
-      <Input
-        style={{ 'width': '100%' }}
-        type="text"
-        disabled={(result === 'false') ? false : true}
-        onFocus={(e) => {
-          e.target.value =  e.target.value 
-        }}
-        onChange={(e) => {
-          let newName = e.target.value
-          editCommand({ name: newName, id: id })
-        }}
-        defaultValue={name}
-      />
-      {commandsCount <= 2 ?
-        <Button size="l"
-          disabled={(result === 'false') ? false : true}
-          appearance="accent" className='score-info' before={(result === 'false') ? <Icon16Cancel /> : `${currentScore}`} />
-        :
-        <Button size="l" className='score-info'
-          disabled={(result === 'false') ? false : true}
-          onClick={() => {
-            removeCommand(id)
+    <div className="command-card">
+      {result === "false" ? (
+        <Input
+          style={{ width: "100%" }}
+          type="text"
+          onFocus={(e) => {
+            e.target.value = e.target.value;
           }}
-          appearance="accent" before={(result === 'false') ? <Icon16Cancel /> : `${currentScore}`} />
-      }
+          onChange={(e) => {
+            let newName = e.target.value;
+            editCommand({ name: newName, id: id });
+          }}
+          defaultValue={name}
+        />
+      ) : (
+        <Card style={{ width: "100%", display:"flex" }}>
+          <p style={{marginLeft:"12px"}}>
+          {name}</p></Card>
+      )}
+
+      {commandsCount <= 2 ? (
+        <Button
+          size="l"
+          disabled={result === "false" ? false : true}
+          appearance="accent"
+          className="score-info"
+          before={result === "false" ? <Icon16Cancel /> : `${currentScore}`}
+        />
+      ) : (
+        <Button
+          size="l"
+          className="score-info"
+          disabled={result === "false" ? false : true}
+          onClick={() => {
+            removeCommand(id);
+          }}
+          appearance="accent"
+          before={result === "false" ? <Icon16Cancel /> : `${currentScore}`}
+        />
+      )}
     </div>
-  )
-}
+  );
+};
